e320ae97e232271c7dd9b85ef3c897dbadc2b3b8
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-21 19:00:13 -0400

    Initial project creation

---
+++ GruntFile.js
@@ -0,0 +1,104 @@
+module.exports = function(grunt)
+{
+  grunt.initConfig({
+    pkg:grunt.file.readJSON('package.json'),
+
+    jade: {
+      debug: {
+        options: {
+          data: {
+            debug: true,
+            timestamp: "<%= grunt.template.today() %>"
+          }
+        },
+        files: {
+          "dist/index.html": "index.jade"
+        },
+      },
+    },
+    sass: {
+      public: {
+        files: [{
+          expand: true,
+          src: ['css/**/*.scss', 'css/**/*.sass'],
+          dest: 'dist',
+          ext: '.css'
+        }],
+      },
+    },
+    jshint: {
+      all: ['js/**/*.js']
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      js: {
+        src: ['js/**/*.js'],
+        dest: 'dist/js/functions.js',
+      },
+      css: {
+        src: ['dist/css/**/*.css'],
+        dest: 'dist/css/style.css',
+      },
+    },
+    uglify: {
+      options: {
+        mangle: {
+          except: ['jQuery']
+        }
+      },
+      my_target: {
+        files: {
+          'dist/js/functions.js': ['js/**/*.js']
+        }
+      },
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      jade: {
+        files: ['index.jade'],
+        tasks: ['jade'],
+      },
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['jshint', 'concat:js'],
+      },
+      sass: {
+        files: ['css/**/*.scss', 'css/**/*.sass'],
+        tasks: ['sass'],
+      },
+      css: {
+        files: ['dist/css/**/*.css'],
+        tasks: ['concat:css'],
+      },
+      files:['dist/**','server/**'],
+      tasks:[],
+    },
+
+    express:{
+        all:{
+            options:{
+                server:'server.js',
+                hostname:'localhost',
+                bases:['./dist'],
+                livereload:true
+            }
+        },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-middleware');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.registerTask('default', ['imagemin', 'jade', 'sass', 'jshint', 'concat', 'uglify']);
+  grunt.registerTask('server',['default', 'express', 'watch']);
+}
---
+++ server/routes/basic.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+    module.exports = function(app) {
+
+       // basic routes to handle request
+        app.get('/', function(req, res) {
+                console.log("Main route");
+                res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
+
+        });
+
+    };
